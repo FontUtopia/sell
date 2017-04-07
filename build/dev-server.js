fa5776888c2ff7aa3154c2463523d350878843cf
niuchaoying <niuchaoying@xuetangx.com>
2017-04-07 19:43:58 +0800

    组件化

--- build/dev-server.js
+++ build/dev-server.js
@@ -14,6 +14,37 @@ var port = process.env.PORT || config.dev.port
 var proxyTable = config.dev.proxyTable
 
 var app = express()
+
+var appData = require('../data.json')
+var seller = appData.seller
+var goods = appData.goods
+var ratings = appData.ratings
+
+// 配置路由
+var apiRoutes = express.Router()
+apiRoutes.get('/seller',function(req,res){
+  res.json({
+    errno: 0,
+    data: seller
+  })
+})
+apiRoutes.get('/goods',function(req,res){
+  res.json({
+    errno: 0,
+    data: goods
+  })
+})
+apiRoutes.get('/ratings',function(req,res){
+  res.json({
+    errno: 0,
+    data: ratings
+  })
+})
+
+// app 是定义过的express对象
+app.use('/api',apiRoutes)
+
+
 var compiler = webpack(webpackConfig)
 
 var devMiddleware = require('webpack-dev-middleware')(compiler, {
--- src/components/header/header.vue
+++ src/components/header/header.vue
@@ -1,2 +1,9 @@
 <template>
+	<div class="header">
+		我是header
+	</div>
 </template>
+<script>
+	export default{}
+</script>
+<style></style>
