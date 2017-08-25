1918bb57938e8a84b1ecc761fe70c24272fb29b4
niuchaoying <niuchaoying@xuetangx.com>
2017-08-25 18:59:55 +0800

    header弹出层布局

--- src/components/header/header.vue
+++ src/components/header/header.vue
@@ -17,37 +17,87 @@
 					<span class="text">{{seller.supports[0].description}}</span>
 				</div>
 			</div>
-			<div v-if="seller.supports" class="support-count">
+			<div v-if="seller.supports" class="support-count" @click="showDetail">
 				<span class="count">{{seller.supports.length}}个</span>
 				<i class="icon-keyboard_arrow_right"></i>
 			</div>
 		</div>
-		<div class="bulletin-wrapper">
+		<div class="bulletin-wrapper" @click="showDetail">
 			<span class="bulletin-title"></span><span class="bulletin-text">{{seller.bulletin}}</span>
 			<i class="icon-keyboard_arrow_right"></i>
 		</div>
 		<div class="background">
 			<img :src="seller.avatar" alt="" width="100%" height="100%">
 		</div>
+		<div v-show="detailShow" class="detail">
+			<div class="detail-wrapper">
+				<div class="detail-main clearfix">
+					<div class="name">{{seller.name}}</div>
+					<div class="start-wrapper">
+						<star :size="48" :score="seller.score"></star>
+					</div>
+					<div class="title">
+						<div class="line"></div>
+						<div class="text">优惠信息</div>
+						<div class="line"></div>
+					</div>
+					<ul v-if="seller.supports" class="supports">
+						<li class="support-item" v-for="item in seller.supports">
+							<span class="icon" :class="classMap[seller.supports[$index].type]"></span>
+							<span class="text">{{seller.supports[$index].description}}</span>
+						</li>
+					</ul>
+					<div class="title">
+						<div class="line"></div>
+						<div class="text">商家公告</div>
+						<div class="line"></div>
+					</div>
+					<div class="bulletin">
+						<p class="content">{{seller.bulletin}}</p>
+					</div>
+				</div>
+			</div>
+			<div class="detail-close" @click="hideDetail">
+				<i class="icon-close"></i>
+			</div>
+		</div>
 	</div>
 </template>
 <script>
+	import star from '../../components/star/star.vue'
 	export default{
 		props:{
 			seller:{
 				type:Object
 			}
 		},
+		data(){
+		  return {
+        detailShow: false
+			}
+		},
+    components:{
+      star
+		},
 		created(){
 			this.classMap = ['decrease','discount','special','invoice','guarantee']
+		},
+		methods:{
+      showDetail(){
+        this.detailShow = true
+			},
+			hideDetail(){
+        this.detailShow = false
+			}
 		}
 	}
 </script>
 <style lang="stylus" rel="stylesheet/stylus">
 	@import "../../common/stylus/mixin"
 	.header
-		color: #fff
 		position : relative
+		overflow : hidden
+		color: #fff
 		background : rgba(7,17,27,0.5)
 		.content-wrapper
 			position : relative
@@ -135,7 +185,7 @@
 				vertical-align : top
 				width : 22px
 				height : 12px
-				margin-top : 7px
+				margin-top : 8px
 				bg-image('bulletin')
 				background-size 22px 12px
 				background-repeat : no-repeat
@@ -156,4 +206,85 @@
 			height : 100%
 			z-index : -1
 			filter : blur(10px)
+		.detail
+			position : fixed
+			top: 0
+			left: 0
+			width : 100%
+			height : 100%
+			z-index : 100
+			overflow : auto
+			background : rgba(7,17,27,0.8)
+			.detail-wrapper
+				width : 100%
+				min-height : 100%
+				.detail-main
+					margin-top : 64px
+					padding-bottom : 64px
+					.name
+						line-height : 16px
+						text-align : center
+						font-size : 16px
+						font-weight : 700
+					.start-wrapper
+						margin-top : 18px
+						padding : 2px 0
+						text-align : center
+					.title
+						display : flex
+						width : 80%
+						margin : 30px auto 24px auto
+						.line
+							flex : 1
+							position : relative
+							top : -6px
+							border-bottom : 1px solid rgba(225,225,225,0.2)
+						.text
+							padding : 0 12px
+							font-size : 14px
+							font-weight : 700
+					.supports
+						width : 80%
+						margin : 0 auto
+						.support-item
+							padding : 0 12px
+							margin-bottom : 12px
+							font-size : 0
+							&:last-child
+								margin-bottom : 0
+							.icon
+								display : inline-block
+								width : 16px
+								height : 16px
+								vertical-align : top
+								margin-right : 6px
+								background-size : 16px 16px
+								background-repeat : no-repeat
+								&.decrease
+									bg-image('decrease_2')
+								&.special
+									bg-image('special_2')
+								&.invoice
+									bg-image('invoice_2')
+								&.guarantee
+									bg-image('guarantee_2')
+								&.discount
+									bg-image('discount_2')
+							.text
+								font-size : 12px
+								line-height : 16px
+					.bulletin
+						width :80%
+						margin : 0 auto
+						.content
+							padding : 0 12px
+							line-height :24px
+							font-size : 12px
+			.detail-close
+				position : relative
+				width : 32px
+				height : 32px
+				margin : -64px auto 0 auto
+				clear : both
+				font-size : 32px
 </style>
