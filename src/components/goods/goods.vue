fd4711279bbef087eaa307a6c436df5d0780959c
niuchaoying <niuchaoying@xuetangx.com>
2017-09-05 19:05:23 +0800

    menulist and foodlist apply to better-scroll

--- src/components/goods/goods.vue
+++ src/components/goods/goods.vue
@@ -1,8 +1,9 @@
 <template>
     <div class="goods">
-			<div class="menu-wrapper">
+			<div class="menu-wrapper" v-el:menu-wrapper>
 				<ul>
-					<li v-for="item in goods" class="menu-item">
+					<li v-for="item in goods" class="menu-item"
+							:class="{'current':currentIndex===$index}" @click="selectMenu($index,$event)">
 						<span class="text border-1px">
 							<span v-show="item.type>0" class="icon" :class="classMap[item.type]"></span>
 							{{item.name}}
@@ -10,9 +11,9 @@
 					</li>
 				</ul>
 			</div>
-			<div class="foods-wrapper">
+			<div class="foods-wrapper" v-el:foods-wrapper>
 				<ul>
-					<li v-for="item in goods" class="food-list">
+					<li v-for="item in goods" class="food-list food-list-hook">
 						<h1 class="title">{{item.name}}</h1>
 						<ul>
 							<li @click="selectFood(food,$event)" v-for="food in item.foods" class="food-item border-1px">
@@ -23,8 +24,7 @@
 									<h2 class="name">{{food.name}}</h2>
 									<p class="desc">{{food.description}}</p>
 									<div class="extra">
-										<span class="count">月售{{food.sellCount}}份</span>
-										<span>好评率{{food.rating}}%</span>
+										<span class="count">月售{{food.sellCount}}份</span><span>好评率{{food.rating}}%</span>
 									</div>
 									<div class="price">
 										<span class="now">¥{{food.price}}</span>
@@ -40,6 +40,7 @@
 		<food :food="selectedFood" v-ref:food></food>
 </template>
 <script type="text/ecmascript-6">
+	import BScroll from 'better-scroll'
 	import food from "components/food/food"
 		const ERR_OK = 0
      export default{
@@ -51,24 +52,71 @@
 			 data(){
 			   return {
 			     goods : [],
+           // 左侧菜单对应的右侧部分高度的区间数组，滚动右侧的时候左侧要出现高亮
+           listHeight: [],
+           scrollY: 0,
            selectedFood : {}
 				 }
 			 },
-			 components:{
-         food
+			 computed: {
+			   currentIndex() {
+			     for(let i=0;i<this.listHeight.length; i++){
+							let height1 = this.listHeight[i]
+						 	let height2 = this.listHeight[i+1]
+						 	if(!height2 || (this.scrollY >= height1 && this.scrollY < height2)){
+							  return i
+							}
+					 }
+					 return 0
+				 }
 			 },
 			 created() {
          this.classMap = ['decrease','discount','special','invoice','guarantee']
 
 			   this.$http.get('/api/goods').then((response)=>{
            response = response.body
-					 if(response.errno===ERR_OK){
+					 if(response.errno === ERR_OK){
              this.goods = response.data
-             console.log(this.goods)
+						 this.$nextTick(() => {
+               // dom更新后再进行初始化滚轮
+               this._initScroll()
+               this._calculateHeight()
+						 })
 					 }
 				 })
 			 },
 			 methods:{
+			   _initScroll(){
+			     this.menuScroll = new BScroll(this.$els.menuWrapper,{
+			       click:true
+					 })
+					 this.foodsScroll = new BScroll(this.$els.foodsWrapper,{
+					   probeType: 3
+					 })
+
+					 this.foodsScroll.on('scroll',(pos) => {
+					   this.scrollY = Math.abs(Math.round(pos.y))
+					 })
+				 },
+				 _calculateHeight(){
+					 let foodlist = this.$els.foodsWrapper.getElementsByClassName('food-list-hook')
+					 let height = 0
+					 this.listHeight.push(height)
+					 for(let i=0;i<foodlist.length;i++){
+						 let item = foodlist[i]
+						 height += item.clientHeight
+						 this.listHeight.push(height)
+					 }
+				 },
+         selectMenu(index,event){
+				   console.log(event)
+				   if(!event._constructed){
+							return
+					 }
+           let foodlist = this.$els.foodsWrapper.getElementsByClassName('food-list-hook')
+					 let el = foodlist[index]
+					 this.foodsScroll.scrollToElement(el,300)
+				 },
          selectFood(food, event){
 //					 if (!event._constructed) {
 //					   return
@@ -76,7 +124,10 @@
 					 this.selectedFood = food
 					 this.$refs.food.show()
 				 }
-			 }
+			 },
+       components:{
+         food
+       }
      }
 </script>
 <style lang="stylus" ref="stylesheet/stylus">
@@ -98,6 +149,14 @@
 				width 56px
 				padding : 0 12px
 				line-height : 14px
+				&.current
+					position: relative
+					z-index : 10
+					margin-top : -1px
+					background : #fff
+					font-weight : 700
+					.text
+						border-none()
 				.icon
 					display: inline-block
 					width: 12px
@@ -157,8 +216,9 @@
 						color: rgb(147,153,159)
 					.desc
 						margin-bottom :8px
+						line-height:12px
 					.extra
-						&.count
+						.count
 							margin-right : 12px
 					.price
 						font-weight :700
