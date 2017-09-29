016a1e95448c0a3b61b002fdb661cfdc954f0d9d
niuchaoying <niuchaoying@xuetangx.com>
2017-09-29 18:28:49 +0800

    shopcart list

--- src/components/shopcart/shopcart.vue
+++ src/components/shopcart/shopcart.vue
@@ -1,6 +1,6 @@
 <template>
 	<div class="shopcart">
-		<div class="content">
+		<div class="content" @click="toggleList">
 			<div class="content-left">
 				<div class="logo-wrapper">
 					<div class="logo" :class="{'highlight':totalCount>0}">
@@ -11,7 +11,7 @@
 				<div class="price" :class="{'highlight':totalPrice>0}">¥{{totalPrice}}元</div>
 				<div class="desc">另需配送费¥{{deliveryPrice}}元</div>
 			</div>
-			<div class="content-right">
+			<div class="content-right" @click.stop.prevent="pay">
 				<div class="pay" :class="payClass">{{payDesc}}</div>
 			</div>
 		</div>
@@ -20,9 +20,31 @@
 					<div class="inner"></div>
 				</div>
 		</div>
+		<div class="shopcart-list" v-show="listShow" transition="fold">
+			<div class="list-header">
+				<h1 class="title">购物车</h1>
+				<span class="empty" @click="empty">清空</span>
+			</div>
+			<div class="list-content" v-el:list-content>
+				<ul>
+					<li class="food" v-for="food in selectFoods">
+						<span class="name">{{food.name}}</span>
+						<div class="price">
+							<span>{{food.price*food.count}}</span>
+						</div>
+						<div class="cartcontrol-wrapper">
+							<cartcontrol :food="food"></cartcontrol>
+						</div>
+					</li>
+				</ul>
+			</div>
+		</div>
 	</div>
+	<div class="list-mask" v-show="listShow" @click="hideList" transition="fade"></div>
 </template>
 <script type="text/ecmascript-6">
+  import BScroll from 'better-scroll'
+  import cartcontrol from "components/cartcontrol/cartcontrol"
 	export default {
     props: {
       selectFoods:{
@@ -59,9 +81,66 @@
             show: false
           }
 				],
-        dropBalls:[]
+        dropBalls:[],
+				fold: true
 			}
 		},
+    components:{
+      cartcontrol
+    },
+    computed: {
+      totalPrice(){
+        let total = 0
+        this.selectFoods.forEach((food)=>{
+          total += food.price * food.count
+        })
+        return total
+      },
+      totalCount(){
+        let count = 0
+        this.selectFoods.forEach((food)=>{
+          count += food.count
+        })
+        return count
+      },
+      payDesc(){
+        if(this.totalPrice === 0){
+          return `¥${this.minPrice}元起送`
+        }else if(this.totalPrice<this.minPrice){
+          let diff = this.minPrice - this.totalPrice
+          return `还差${diff}元起送`
+        }else{
+          return '去结算'
+        }
+      },
+      payClass(){
+        if(this.totalPrice<this.minPrice){
+          return 'not-enough'
+        }else{
+          return 'enough'
+        }
+      },
+			listShow(){
+        if(!this.totalCount){
+          this.fold = true
+					return false
+				}
+				let show = !this.fold
+				if(show){
+          // 只在购物车列表展示的时候才初始化滚轮～哈哈哈，you know。。。
+					if(!this.scroll){
+					  // 注意啦 scroll不存在的时候在新建哦，祖国教育我们不要浪费资源～
+            this.scroll = new BScroll(this.$els.listContent,{
+              click:true
+            })
+					}else{
+					  // BScroll有一个刷新方法，scroll存在的时候，我们直接调用刷新方法就可以啦～
+            this.scroll.refresh()
+					}
+				}
+				return show
+			}
+    },
 		methods:{
 		  drop(el) {
 				for(let i=0;i<this.balls.length;i++){
@@ -73,6 +152,29 @@
 						return
 					}
 				}
+			},
+      toggleList(){
+		    if(!this.totalCount){
+		      return
+				}
+				this.fold = !this.fold
+			},
+      empty() {
+        // this.selectFoods = [] 你要这么写，未免考虑的太不周全了吧，cartcontrol组件的数量还在啊。。。
+				// 正确的姿势～
+        this.selectFoods.forEach((food) => {
+					food.count = 0
+				})
+			},
+      hideList() {
+        this.fold = true
+			},
+      pay() {
+        // 注意啦～这里要写阻止事件冒泡哦 vue阻止事件写法－ @click.stop.prevent
+        if(this.totalPrice < this.minPrice){
+          return
+				}
+				window.alert(`支付${this.totalPrice}元`)
 			}
 		},
 		transitions:{
@@ -112,43 +214,11 @@
 					}
 				}
 			}
-		},
-		computed: {
-      totalPrice(){
-        let total = 0
-				this.selectFoods.forEach((food)=>{
-          total += food.price * food.count
-				})
-				return total
-			},
-			totalCount(){
-        let count = 0
-				this.selectFoods.forEach((food)=>{
-          count += food.count
-				})
-				return count
-			},
-			payDesc(){
-			  if(this.totalPrice === 0){
-			    return `¥${this.minPrice}元起送`
-				}else if(this.totalPrice<this.minPrice){
-			    let diff = this.minPrice - this.totalPrice
-			    return `还差${diff}元起送`
-				}else{
-				  return '去结算'
-				}
-			},
-			payClass(){
-			  if(this.totalPrice<this.minPrice){
-			    return 'not-enough'
-				}else{
-			    return 'enough'
-				}
-			}
 		}
 	}
 </script>
 <style lang="stylus" ref="stylesheet/stylus">
+	@import "../../common/stylus/mixin.styl"
 	.shopcart
 		position : fixed
 		left: 0
@@ -252,4 +322,71 @@
 						border-radius : 50%
 						background-color : rgb(0,160,220)
 						transition : all 0.4s linear
+		.shopcart-list
+			position: absolute
+			top : 0
+			left : 0
+			z-index: -1
+			width : 100%
+			&.fold-transition
+				transition : all 0.5s
+				transform : translate3d(0,-100%,0)
+			&.fold-enter,&.fold-leave
+				transform : translate3d(0,0,0)
+			.list-header
+				height : 40px
+				line-height : 40px
+				padding : 0 18px
+				background : #f3f5f7
+				border-bottom : 1px solid rgba(7,17,27,0.1)
+				.title
+					float : left
+					font-size : 14px
+					color : rgb(7,17,27)
+				.empty
+					float: right
+					font-size: 12px
+					color : rgb(0,160,220)
+			.list-content
+				padding : 0 18px
+				max-height: 217px
+				overflow : hidden
+				background : #fff
+				.food
+					position : relative
+					padding : 12px 0
+					box-sizing : border-box
+					border-1px(rgba(7,17,27,0.1))
+					.name
+						line-height : 24px
+						font-size : 14px
+						color : rgb(7,17,27)
+					.price
+						position : absolute
+						right :90px
+						bottom : 12px
+						line-height : 24px
+						font-size: 14px
+						font-weight : 700
+						color : rgb(240,20,20)
+					.cartcontrol-wrapper
+						position: absolute
+						right : 0
+						bottom: 6px
+	.list-mask
+		position: fixed
+		top : 0
+		left : 0
+		width : 100%
+		height : 100%
+		z-index: 40
+		backdrop-filter : blur(10px)
+		&.fade-transition
+			transition : all 0.5s
+			opacity : 1
+			background : rgba(7,17,27,0.6)
+		&.fade-enter,&.fade-leave
+			// 只有在fade-transition有 transition : all 0.5s 属性的时候这两个样式才有用～
+			opacity : 0
+			background : rgba(7,17,27,0)
 </style>
